8cbfb3147e8fbba2e506e3a11fed0cfb4a5913bf
Musaib Altaf <85894666+musaib-js@users.noreply.github.com>
2024-06-10 18:57:01 +0530

    Update index.js

--- index.js
+++ index.js
@@ -82,6 +82,32 @@ app.post('/get-supervisor-details', (req, res) => {
 }
 )
 
+app.post('/get-team-workflows', (req, res) => {
+    try {
+        let team_id= req.body.team_id;
+        let organisation_id = req.body.org_id
+        # fetch all the worklows of the particular team org pair
+        let workflows = [{}, {}, {}]
+        if (workflows.length === 0){
+            res.send({
+                status: "error",
+                message: "No workflows not found"
+            })
+        }
+        res.send({
+            workflows: workflows,
+            status: "success",
+            message: "Worflows fetched successfully"
+        })
+    } catch (error) {
+        res.send({
+            status: "error",
+            message: "Something went wrong"
+        })
+    }  
+}
+)
+
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
 })
